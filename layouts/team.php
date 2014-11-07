9f05de5dd0bfce1ea770248ed01c4bcced3ad37a
WetHippo <m1a2r3k4o5@hot.ee>
2014-11-07 02:58:37 +0200

    ülesandega 3 midagi saavutatud, võibolla 3 käes?

--- layouts/team.php
+++ layouts/team.php
@@ -3,11 +3,9 @@
 <head>
   <?php include "../components/html-head.php"; ?>
 </head>
-
 <body>
   <div id="site-container">
     <?php include "../components/site-header.php"; ?>
-
     <main id="site-body">
       <section id="site-content">
         <div class="team-member">
@@ -15,31 +13,26 @@
           <div class="member-info"><span>E-mail: </span><a href="mailto:kaspar.naaber@khk.ee">kaspar.naaber@khk.ee</a></div>
           <div class="member-info"><span>Huvialad: </span>Arvutid</div>
         </div>
-
         <div class="team-member">
           <h2 class="member-name">Peeter Kartul</h2>
           <div class="member-info"><span>E-mail: </span><a href="mailto:peeter.kartul@khk.ee">peeter.kartul@khk.ee</a></div>
           <div class="member-info"><span>Huvialad: </span>Jalgrattad</div>
         </div>
-
         <div class="team-member">
           <h2 class="member-name">Erki Nool</h2>
           <div class="member-info"><span>E-mail: </span><a href="mailto:erki.nool@khk.ee">erki.nool@khk.ee</a></div>
           <div class="member-info"><span>Huvialad: </span>Jalgpall</div>
         </div>
-
         <div class="team-member">
           <h2 class="member-name">Timo Tigu</h2>
           <div class="member-info"><span>E-mail: </span><a href="mailto:timo.tigu@khk.ee">timo.tigu@khk.ee</a></div>
           <div class="member-info"><span>Huvialad: </span>Raamatud</div>
         </div>
-
         <div class="team-member">
           <h2 class="member-name">Silver Kuu</h2>
           <div class="member-info"><span>E-mail: </span><a href="mailto:silver.kuu@khk.ee">silver.kuu@khk.ee</a></div>
           <div class="member-info"><span>Huvialad: </span>Peod</div>
         </div>
-
         <div class="team-member">
           <h2 class="member-name">Tauno Päike</h2>
           <div class="member-info"><span>E-mail: </span><a href="mailto:tauno.paike@khk.ee">tauno.paike@khk.ee</a></div>
@@ -47,7 +40,6 @@
         </div>
       </section>
     </main>
-
     <?php include "../components/site-footer.php"; ?>
   </div>
 </body>
